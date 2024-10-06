f4201dabb03bce4d7950fcb8afa20de3015183a1
nader.zare <nader.zare88@gmail.com>
2024-10-06 15:11:26 -0300

    remove sub title

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'RoboCup Soccer Simulation 2D',
-  tagline: 'Dinosaurs are cool',
+  tagline: '',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
