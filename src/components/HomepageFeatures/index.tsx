5590771fd82ac55b2efcde8c92837fa36d1114f0
Nader Zare <nader.zare88@gmail.com>
2024-12-23 11:58:19 -0400

    Develop news (#23)
    * Add new blog entries for RoboCup Soccer Simulation 2D League
    
    * upgrade blog-list-plugin
    
    * add FeatureList to home page and remove text
    
    * update news page (blog page) title size
    
    * add two images for two news (blog)
    
    * update blog sidebar title and navigation label to 'News'
    
    * Change How to add news location
    
    ---------
    
    Co-authored-by: SK2iP <sadra.khanjari@gmail.com>

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,51 +1,12 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import News from './myblogs';
 import React from 'react';
+import FeatureList from './featureBlogs';
 
-
-type FeatureItem = {
-  title: string;
-  image: string;
-  description: JSX.Element;
-  link?: string;
-};
-
-const FeatureList: FeatureItem[] = [
-  
-];
 
 function MainPageText() {
   return (
-    <div>
-      <h2>About the RoboCup Soccer Simulator</h2>
-      <p>The RoboCup Soccer Simulator is a research and educational tool for multiagent systems and artificial intelligence. It enables for two teams of 11 simulated autonomous robotic players to play soccer (football).</p>
-      <h2>League Overview</h2>
-      <p>
-        Without the neccessity to maintain any robot hardware, the RoboCup Simulation League’s focus comprises artificial intelligence and team strategy.
-        In the 2D Simulation League, two teams of eleven autonomous software programs (called agents) each play soccer in a two-dimensional virtual soccer stadium represented by a central server, called SoccerServer. This server knows everything about the game, i.e. the current position of all players and the ball, the physics and so on. The game further relies on the communication between the server and each agent. On the one hand each player receives relative and noisy input of his virtual sensors (visual, acustic and physical) and may on the other hand perform some basic commands (like dashing, turning or kicking) in order to influence its environment.
-        The big challenge in the Simulation League is to conclude from all possible world states (derived from the sensor input by calculating a sight on the world as absolute and noise-free as possible) to the best possible action to execute. As a game is divided into 6000 cycles this task has to be accomplished in time slot of 100 ms (the length of each cycle). Further information and the SoccerServer software can be accessed via <a href='https://github.com/rcsoccersim/'>https://github.com/rcsoccersim/</a>.
-      </p>
-      <h3>Upcoming Events</h3>
-      <ul>
-        <li><a href='https://2025.robocup.org/'>RoboCup2025</a></li>
-      </ul>
-    </div>
-  );
-}
-
-function Feature({ title, image, description, link }: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <img src={image} alt={title} className={styles.featureSvg} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <a href={link}><Heading as="h3">{title}</Heading></a>
-        <p>{description}</p>
-      </div>
-    </div>
+    <div></div>
   );
 }
 
@@ -61,9 +22,7 @@ export default function HomepageFeatures(): JSX.Element {
         <div className="container">
           <MainPageText />
           <div className="row">
-            {FeatureList.map((props, idx) => (
-              <Feature key={idx} {...props} />
-            ))}
+            <FeatureList />
           </div>
         </div>
 
